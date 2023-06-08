d1d8d6ea1736822d6313c16ba6d4ddc5a93e018c
gabrielangomez <gomez.n.gabriela@gmail.com>
2023-06-08 18:44:35 -0300

    preEntrega” git commit -m preEntrega”

--- js.js
+++ js.js
@@ -1,36 +1,27 @@
-
-function seleccionarUnModeloDeInflable(opcion) {
-    let precio;
-
-    switch (opcion) {
-        case "Modelo Inflable Punta Lápiz":
-          precio = 8500;
-          console.log("Has seleccionado el Modelo de Inflable Punta Lápiz");
-          break;
-        case "Modelo Inflable Punta Palmera":
-          precio = 8.500;
-          console.log("Has seleccionado el Modelo de Inflable Punta Palmera");
-          break;
-        case "Modelo Inflable Rampa":
-          precio = 12000;
-          console.log("Has seleccionado el Modelo de Inflable Rampa");
-          break;
-        default:
-          console.log("Seleccionar un modelo");
-          return;
-      }
-
-    console.log("El precio del producto es: $" + precio);
-
-    for (let i = 0; i < 4; i++) {
-        console.log("Iteración " + (i + 1));
-      }
+function seleccionarUnModeloDeInflable() {
+  let opcion = prompt("Seleccione un modelo de inflable:\n1. Modelo Inflable Punta Lápiz\n2. Modelo Inflable Punta Palmera\n3. Modelo Inflable Rampa");
+  let precio;
+  switch (opcion) {
+    case "1":
+      precio = 8500;
+      console.log("Has seleccionado el Modelo de Inflable Punta Lápiz");
+      break;
+    case "2":
+      precio = 8500;
+      console.log("Has seleccionado el Modelo de Inflable Punta Palmera");
+      break;
+    case "3":
+      precio = 12000;
+      console.log("Has seleccionado el Modelo de Inflable Rampa");
+      break;
+    default:
+      console.log("Opción inválida");
+      return;
   }
+  console.log("El precio del producto es: $" + precio);
 
-    seleccionarUnModeloDeInflable("Modelo Inflable Punta Lápiz");
-    seleccionarUnModeloDeInflable("Modelo Inflable Punta Palmera");
-    seleccionarUnModeloDeInflable("Modelo Inflable Rampa");
-  
-
-
- 
+  for (let i = 0; i < 4; i++) {
+    console.log("Iteración " + (i + 1));
+  }
+}
+seleccionarUnModeloDeInflable();
